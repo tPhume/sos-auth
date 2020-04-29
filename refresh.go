6e2408cefe600a16c8088527724579fc1904cfdb
Traphume <traphumedev@gmail.com>
2020-04-29 20:09:30 +0700

    Added RefreshResponse and Secret attribute to RefreshHandler

--- refresh.go
+++ refresh.go
@@ -9,12 +9,17 @@ type Refresh struct {
 	RefreshToken string `json:"refresh_token" binding:"required,uuid4"`
 }
 
+type RefreshResponse struct {
+	Token string `json:"token"`
+}
+
 type CheckToken interface {
 	Check(ctx context.Context, token string) error
 }
 
 type RefreshHandler struct {
 	CheckToken CheckToken
+	Secret     string
 }
 
 func (rh *RefreshHandler) Refresh(ctx *gin.Context) {
