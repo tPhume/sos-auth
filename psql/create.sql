70abdb657bfea8247c4c40fe5b12dd4ad624892d
Traphume <traphumedev@gmail.com>
2020-04-29 23:33:41 +0700

    Fixed psql

--- psql/create.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE Account
-(
-    user_id  serial PRIMARY KEY,
-    role     VARCHAR(50)        NOT NULL,
-    name     VARCHAR(50)        NOT NULL,
-    email    VARCHAR(50) UNIQUE NOT NULL,
-    password VARCHAR(20)        NOT NULL,
-    address  VARCHAR(50)
-);
