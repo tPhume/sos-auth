80b0c36c46deb39443c81371138c104efaee0359
Traphume <traphumedev@gmail.com>
2020-04-29 20:35:47 +0700

    Implemented RerfreshHandler

--- refresh.go
+++ refresh.go
@@ -2,7 +2,10 @@ package sos_auth
 
 import (
 	"context"
+	"errors"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 // Request body for refresh endpoint
@@ -21,8 +24,11 @@ type RefreshData struct {
 	Role   string `json:"role"`
 }
 
+// Interacts with refresh token data source
+var refreshNoMatch = errors.New("refresh token does not exist")
+
 type CheckToken interface {
-	Check(ctx context.Context, token string) (RefreshData, error)
+	Check(ctx context.Context, token string) (*RefreshData, error)
 }
 
 type RefreshHandler struct {
@@ -31,5 +37,33 @@ type RefreshHandler struct {
 }
 
 func (rh *RefreshHandler) Refresh(ctx *gin.Context) {
-	panic("implement me")
+	// Get request body
+	body := &RefreshBody{}
+	if err := ctx.ShouldBindJSON(body); err != nil {
+		ctx.String(http.StatusBadRequest, "invalid format")
+		return
+	}
+
+	// Check refresh token
+	data, err := rh.CheckToken.Check(ctx, body.RefreshToken)
+	if err != nil {
+		if err == refreshNoMatch {
+			ctx.String(http.StatusNotFound, "no match")
+		} else {
+			ctx.String(http.StatusInternalServerError, "internal error when checking refresh token")
+		}
+
+		return
+	}
+
+	// Create JWT token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": data.UserId,
+		"role":    data.Role,
+	})
+
+	tokenString, err := token.SignedString(rh.Secret)
+
+	// Create Response
+	ctx.JSON(http.StatusCreated, RefreshResponse{Token: tokenString})
 }
