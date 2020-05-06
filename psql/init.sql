cf4126a210b4e6d9b55b6f4390d822c5641e08d4
Traphume <traphumedev@gmail.com>
2020-05-06 22:43:51 +0700

    Reformat code

--- psql/init.sql
+++ psql/init.sql
@@ -1,12 +1,13 @@
 CREATE TABLE "Users"
 (
-    id  serial PRIMARY KEY,
+    id       serial PRIMARY KEY,
     role     VARCHAR(50)        NOT NULL,
-    username     VARCHAR(50)        NOT NULL,
+    username VARCHAR(50)        NOT NULL,
     email    VARCHAR(50) UNIQUE NOT NULL,
-    password VARCHAR(100)        NOT NULL,
+    password VARCHAR(100)       NOT NULL,
     address  VARCHAR(50)
 );
 
 INSERT INTO "Users" (role, username, email, password, address)
-VALUES ('user', 'Jack', 'jack@supermail.com', '$2y$10$MmgAdnJGQBlI8P42Cukn4u9RpGzhtHMjg6b3Tq9Zcp2Fin/lWmMW2', 'Jack Street 42, Bangkok, Thailand');
+VALUES ('user', 'Jack', 'jack@supermail.com', '$2y$10$MmgAdnJGQBlI8P42Cukn4u9RpGzhtHMjg6b3Tq9Zcp2Fin/lWmMW2',
+        'Jack Street 42, Bangkok, Thailand');
