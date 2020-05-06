4ef1ed66796ae6db6fe9405f960d10cf2a3f0648
Traphume <traphumedev@gmail.com>
2020-05-06 22:09:53 +0700

    Update something lol

--- psql/init.sql
+++ psql/init.sql
@@ -8,5 +8,5 @@ CREATE TABLE "Users"
     address  VARCHAR(50)
 );
 
-INSERT INTO "User" (role, username, email, password, address)
+INSERT INTO "Users" (role, username, email, password, address)
 VALUES ('user', 'Jack', 'jack@supermail.com', '$2y$10$MmgAdnJGQBlI8P42Cukn4u9RpGzhtHMjg6b3Tq9Zcp2Fin/lWmMW2', 'Jack Street 42, Bangkok, Thailand');
